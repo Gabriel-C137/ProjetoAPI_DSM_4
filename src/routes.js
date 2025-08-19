0d9054dd4b48274840e1438a8e65496609052b09
GABRIEL CAMARA DE OLIVEIRA <1091392413019@fatec-srv.local>
2025-08-18 21:12:32 -0300

    Inserção e desenvolvimento do Login - 18-08

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,32 @@ import react from 'react'
 import { NavigationContainer } from '@react-navigation/native' 
 import { createStackNavigator } from '@react-navigation/stack'
 
-import Main from ''
+import Main from './pages/main'
+import Login from './pages/login'
+import User from './pages/user'
+
+const Stack = createStackNavigator()
+
+export default function Routes(){
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="login"
+                    component={Login}
+                    options={{
+                        title: 'Login',
+                        headerTitleAlign: 'center',
+                        headerStyle:{
+                            backgroundColor: '#3498DB',
+                        },
+                        headerTitleStyle:{
+                            fontWeight: 'bold',
+                            color: '#fff'
+                        }
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
